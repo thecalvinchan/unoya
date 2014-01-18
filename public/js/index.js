c510253f81f3dc3387f74d7ef837bc4f61337efd
Calvin <calvin.chan.h@gmail.com>
2014-01-17 21:09:11 -0500

    Re-added javascript files

---
+++ public/js/index.js
@@ -0,0 +1,14 @@
+document.addEventListener('scroll',function(){
+    var height = $('#signup').height();
+    if (window.scrollY >= height+1) {
+        $('#header').css({
+            'position':'absolute',
+            'top':height-300
+        });
+    } else {
+        $('#header').css({
+            'position':'fixed',
+            'top':0
+        });
+    }
+});
